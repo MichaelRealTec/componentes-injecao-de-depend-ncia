ca09abf9bb0b9d06d95bb35e5a09bc09529e0ecc
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-11-08 21:37:59 -0300

    Primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/* Exemplo didático
+		Fazer um programa ler os dados de um funcionário(nome, salário bruto) e depois mostrar o salário líquido,
+		que consiste no valor bruto descontado impostos e previdência.
+		Regras:
+		1) imposto é 20%
+		2) Previdencia é 10%
+		
+		Exemplo:
+		Nome: MARIA
+		Salário bruto: 4000.00
+		Salário liquido = 2800.00
+		*/
+		
+		
+		
+	}
+
+}
